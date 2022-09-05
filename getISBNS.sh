5803ec791d3aa2a434844c39c9179e2c03da2b27
Charles Horn <charles.horn@gmail.com>
2022-09-06 11:04:24 +1200

    split out Link issues into manual link and 'other'

--- getISBNS.sh
+++ getISBNS.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
+# Script to fetch Wikipedia ISBN errors page and history
+# and process the data to TSV for spreadsheet tabulation and charting.
+
 datetag=$(date +'%Y%m%d')
+historyLimit=20
+topArticleLimit=200
+
 wget "https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Check_Wikipedia/ISBN_errors" -O ISBN_errors_${datetag}.html
-wget "https://en.wikipedia.org/w/index.php?title=Wikipedia:WikiProject_Check_Wikipedia/ISBN_errors&action=history&limit=20" -O ISBN_errors_history_${datetag}.html
+wget "https://en.wikipedia.org/w/index.php?title=Wikipedia:WikiProject_Check_Wikipedia/ISBN_errors&action=history&limit=$historyLimit" -O ISBN_errors_history_${datetag}.html
 
 echo "Classes of ISBN issues (by first character)"
-egrep -o '<li>[0-9]+ x ISBN..' <(egrep -v "ISBN\]\].\[\[" ISBN_errors_${datetag}.html) | sed 's/<li>[0-9]\+ x ISBN//' | sort | uniq -c | sed "s/^\s*\([0-9]\+\)/\1\t/;s/=/'=/"
+manualLinks=$(fgrep -c 'x ISBN [[ISBN (identifier)|ISBN]]' ISBN_errors_${datetag}.html)
+classes=$(egrep -o '<li>[0-9]+ x ISBN..' ISBN_errors_${datetag}.html | sed 's/<li>[0-9]\+ x ISBN//' | sort | uniq -c | sed "s/^\s*\([0-9]\+\)/\1\t/;s/=/'=/")
+rawLinks=$(echo "$classes" | fgrep '[' | cut -f1)
+otherLinks=$(($rawLinks - 2 * $manualLinks))
+
+echo -e "Manual ISBN links\t$manualLinks"
+echo -e "Other links\t$otherLinks"
+echo "$classes" | fgrep -v '['
 
 echo
 echo "Top articles with ISBN issues:"
-egrep '<li>[0-9]+ x ISBN..' <(cat ISBN_errors_${datetag}.html) | egrep -o "\">[^<]*</a>" | sed 's/">//;s/<\/a>//' | sort | uniq -c | sed 's/^\s*\([0-9]\+\)/\1\t/' | sort -rg | head -n200
+egrep '<li>[0-9]+ x ISBN..' <(cat ISBN_errors_${datetag}.html) | egrep -o "\">[^<]*</a>" | sed 's/">//;s/<\/a>//' | sort | uniq -c | sed 's/^\s*\([0-9]\+\)/\1\t/' | sort -rg | head -n$topArticleLimit
 
 
 echo
