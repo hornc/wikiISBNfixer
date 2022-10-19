9e5ececdcffadf17f9a20ab78040632d71393326
Charles Horn <charles.horn@gmail.com>
2022-10-19 13:55:37 +1300

    fix typo

--- afclip.sh
+++ afclip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copy content result of article_fix.py scipt to clipboard
+# Copy content result of article_fix.py script to clipboard
 # (Requires xclip)
 
 ./article_fix.py "$@" | tee >(tail -n+2 | head -n-2 | xclip -selection clipboard)
