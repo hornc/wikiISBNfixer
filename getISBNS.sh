87f9d2566591b94b663ad17bf7df65f31ca9bf89
Chales Horn <charles.horn@gmail.com>
2022-11-10 11:21:02 +1300

    don't download pages if file already exists

--- getISBNS.sh
+++ getISBNS.sh
@@ -6,9 +6,10 @@ datetag=$(date +'%Y%m%d')
 historyLimit=20
 topArticleLimit=200
 
-wget "https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Check_Wikipedia/ISBN_errors" -O ISBN_errors_${datetag}.html
-wget "https://en.wikipedia.org/w/index.php?title=Wikipedia:WikiProject_Check_Wikipedia/ISBN_errors&action=history&limit=$historyLimit" -O ISBN_errors_history_${datetag}.html
+wget -nc "https://en.wikipedia.org/wiki/Wikipedia:WikiProject_Check_Wikipedia/ISBN_errors" -O ISBN_errors_${datetag}.html
+wget -nc "https://en.wikipedia.org/w/index.php?title=Wikipedia:WikiProject_Check_Wikipedia/ISBN_errors&action=history&limit=$historyLimit" -O ISBN_errors_history_${datetag}.html
 
+echo
 echo "Classes of ISBN issues (by first character)"
 manualLinks=$(fgrep -c 'x ISBN [[ISBN (identifier)|ISBN]]' ISBN_errors_${datetag}.html)
 classes=$(egrep -o '<li>[0-9]+ x ISBN..' ISBN_errors_${datetag}.html | sed 's/<li>[0-9]\+ x ISBN//' | sort | uniq -c | sed "s/^\s*\([0-9]\+\)/\1\t/;s/=/'=/")
